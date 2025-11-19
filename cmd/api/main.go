eb1f5d43b8e634a328f9dd3c02109132d1d9ac10
Atefe <aa.asgariyan12@gmail.com>
2025-11-19 13:34:42 +0330

    feat: add a gui to add variables

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,14 +4,21 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"mssql-api/internal/config"
 	"mssql-api/internal/database"
 	"mssql-api/internal/handlers"
 	"mssql-api/internal/repository"
+
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
+	showConfigGUI()
+
 	// Load configuration
 	cfg, err := config.Load()
 	if err != nil {
@@ -52,4 +59,49 @@ func main() {
 	if err := http.ListenAndServe(addr, mux); err != nil {
 		log.Fatalf("Server failed to start: %v", err)
 	}
+}
+
+func showConfigGUI() {
+	a := app.New()
+	w := a.NewWindow("Configure Database")
+
+	server := widget.NewEntry()
+	server.SetPlaceHolder("DB_SERVER")
+
+	dbName := widget.NewEntry()
+	dbName.SetPlaceHolder("DB_NAME")
+
+	user := widget.NewEntry()
+	user.SetPlaceHolder("DB_USER")
+
+	pass := widget.NewPasswordEntry()
+	pass.SetPlaceHolder("DB_PASSWORD")
+
+	port := widget.NewEntry()
+	port.SetPlaceHolder("SERVER_PORT")
+	port.SetText("8080") // default
+
+	save := widget.NewButton("Save & Start Server", func() {
+		f, err := os.Create(".env")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer f.Close()
+		fmt.Fprintf(f, "DB_SERVER=%s\nDB_NAME=%s\nDB_USER=%s\nDB_PASSWORD=%s\nSERVER_PORT=%s\n",
+			server.Text, dbName.Text, user.Text, pass.Text, port.Text)
+
+		w.Close() // close GUI and continue to start server
+	})
+
+	w.SetContent(container.NewVBox(
+		server,
+		dbName,
+		user,
+		pass,
+		port,
+		save,
+	))
+
+	w.ShowAndRun()
 }
