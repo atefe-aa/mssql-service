9acde64e5d9c024a985a29db999dc65bdc5e553f
Atefe <aa.asgariyan12@gmail.com>
2025-11-20 11:57:00 +0330

    fix: log the error

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -17,6 +17,13 @@ import (
 )
 
 func main() {
+	logFile, err := os.OpenFile("app.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err == nil {
+		log.SetOutput(logFile)
+	} else {
+		log.Println("Failed to open log file:", err)
+	}
+
 	showConfigGUI()
 
 	// Load configuration
